51c89014b502b3f933358c3c8872870744df3747
itamar Arjuan <itamar.arjuan@gmail.com>
2018-10-29 12:18:49 +0200

    debugging ci

--- ops/setup-jepsen.sh
+++ ops/setup-jepsen.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e # exit on an error
 
-#$JEPSEN_ROOT=pwd
-echo pwd
-echo "JEPSEN_ROOT set to: $JEPSEN_ROOT"
+
+echo $0
+#echo "JEPSEN_ROOT set to: $JEPSEN_ROOT"
 exit 0
 cd docker
 sh up.sh --daemon
